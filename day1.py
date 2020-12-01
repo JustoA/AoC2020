c4eb556ca1506db36d2d2ce5153ddd0f8dc3bd56
Justin Aquilante <jwaquilante@wpi.edu>
2020-12-01 11:49:36 -0500

    a

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 import inp
 
-prompt = inp.parsefile()
+prompt = inp.parse_file_int()
 
 # i accidentally deleted all my files trying to add them to git (don't ask) so have a really crappy solution for today
 for i in prompt:
@@ -8,4 +8,4 @@ for i in prompt:
         for k in prompt:
             if i+j+k == 2020:
                 print(i*j*k)
-                exit(11037)
+                exit(11037)  # im quirky and exit code 0 is for losers
